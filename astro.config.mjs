a9d3c19d7a4abd452834a89e36ed6ff0374cf74e
niclastokerud <niclas.tokerud@gmail.com>
2025-11-26 14:29:42 +0100

    1. Added sanity to project 2. Added schema to sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import sanity from '@sanity/astro';
 import node from '@astrojs/node';
+import react from '@astrojs/react'; // Denne må kanskje legges til manuelt hvis den mangler
 
-// https://astro.build/config
 export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'yhvccqet', // SFA-prosjektet.
+      dataset: 'production',
+      useCdn: true, // Raskere, men kan ta noen minutter før endringer vises
+      studioBasePath: '/admin', // Her vil admin-panelet ditt leve
+    }),
+    react(), // Sanity Studio trenger React
+  ],
+  output: 'server',
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: 'standalone',
+  }),
 });
